d2374314eab57edcf5d3365697b0d6193254ff77
smoka <sachhariel.moka@my.jcu.edu.au>
2023-10-24 18:58:59 +1000

    Complete programming_language.py and languages.py

---
+++ prac_06/languages.py
@@ -0,0 +1,17 @@
+"""
+CP1404/CP5632 Practical - Client code to use the Car class.
+Note that the import has a folder (module) in it.
+This is why we name our folders with no spaces or capitals, as valid module names.
+"""
+from prac_06.programming_language import ProgrammingLanguage
+
+python = ProgrammingLanguage("Python", "Dynamic", True, 1991)
+ruby = ProgrammingLanguage("Ruby", "Dynamic", True, 1995)
+visual_basic = ProgrammingLanguage("Visual Basic", "Static", False, 1991)
+print(python)
+
+languages = [python, ruby, visual_basic]
+print("The dynamically typed languages are: ")
+for language in languages:
+    if language.is_dynamic():
+        print(language.name)
---
+++ prac_06/programming_language.py
@@ -0,0 +1,29 @@
+"""
+CP1404/CP5632 Practical
+Programming Languages
+
+Expected Time: 25 Minutes
+Actual Time:
+"""
+
+
+class ProgrammingLanguage:
+    """Represent a ProgrammingLanguage Object"""
+
+    def __init__(self, name="", typing="", reflection=False, year=0):
+        """Initialise a Programming Langauge Instance
+
+        language_name: str, name of Programming Language
+        typing: str, typing style of Programming Language
+        year: int, year of Programming Language's first release
+        """
+        self.name = name
+        self.typing = typing
+        self.reflection = reflection
+        self.year = year
+
+    def __str__(self):
+        return f"{self.name}, {self.typing} Typing, Reflection={self.reflection}, First appeared in {self.year}"
+    def is_dynamic(self):
+        """Return whether a Programming Language is Dynamic or not"""
+        return self.typing == "Dynamic"
