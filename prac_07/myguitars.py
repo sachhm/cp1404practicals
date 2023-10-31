cc82128867778a1b278e3b1b704f6f21d4a8a478
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2023-10-31 13:21:13 +1000

    Complete More Guitars exercise

--- prac_07/myguitars.py
+++ prac_07/myguitars.py
@@ -15,17 +15,33 @@ FILENAME = "guitars.csv"
 # using the class that you wrote recently.
 guitars = []
 in_file = open(FILENAME, 'r')
-in_file.readline()
 reader = csv.reader(in_file)  # use default dialect, Excel
 for row in reader:
     guitar = Guitar(row[0], int(row[1]), float(row[2]))
     guitars.append(guitar)
 in_file.close()
 
-# Sort guitars
+# Get guitars, store in list and write
+name = input("Name: ")
+
+while name != "":
+    year = int(input("Year: "))
+    cost = float(input("Cost: $"))
+    guitar = Guitar(name, year, cost)
+    guitars.append(guitar)
+    print(f"{guitar} added.")
+    name = input("Name: ")
+
 guitars.sort()
 
-# Display using a loop
-for guitar in guitars:
-    print(guitar)
+for i, guitar in enumerate(guitars, 1):
+    vintage_string = "(vintage)" if guitar.is_vintage() else ""
+    print(f"Guitar {i}: {guitar.name:>30} ({guitar.year}), worth ${guitar.cost:10,.2f} {vintage_string}")
 
+out_file = open(FILENAME, 'w')
+out_file.write("")
+for guitar in guitars:
+    guitar_to_write = f"{guitar.name},{guitar.year},{guitar.cost}"
+    out_file.write(guitar_to_write + "\n")
+#
+# out_file.close()
