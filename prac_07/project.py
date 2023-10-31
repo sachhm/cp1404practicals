3e554d4ce317a9728d19211a0faf5833544630a6
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2023-10-31 15:02:20 +1000

    Add __str__ for Project

--- prac_07/project.py
+++ prac_07/project.py
@@ -6,12 +6,14 @@ Estimated: 1 hour
 Actual:
 """
 
+
 class Project:
     """Represent a Project"""
+
     def __init__(self, name="", start_date="", priority=0, cost_estimate=0, completion_percentage=0):
         """Initialise a Project"""
         self.name = name
-        self.start_date =  start_date
+        self.start_date = start_date
         self.priority = priority
         self.cost_estimate = cost_estimate
         self.completion_percentage = completion_percentage
