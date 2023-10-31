3dd9f0d19ac0a768b2a661967465182733137fa4
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2023-10-31 14:49:42 +1000

    Complete basic menu layout

--- prac_07/project_management.py
+++ prac_07/project_management.py
@@ -7,8 +7,7 @@ Actual:
 
 """
 
-MENU = """
-- (L)oad projects  
+MENU = """- (L)oad projects  
 - (S)ave projects  
 - (D)isplay projects  
 - (F)ilter projects by date
@@ -17,33 +16,65 @@ MENU = """
 - (Q)uit
 """
 
+
 def main():
-    """Main program for Project Managment"""
-    pass
+    """Main program for Project Management"""
+    print(MENU)
+    menu_choice = input(">>>").upper()
+    while menu_choice != "Q":
+        if menu_choice == "L":
+            filename = input("Filename: ")
+            print(load_projects(filename))
+        elif menu_choice == "S":
+            filename = input("Filename: ")
+            save_projects(filename)
+        elif menu_choice == "D":
+            pass
+        elif menu_choice == "F":
+            pass
+        elif menu_choice == "A":
+            pass
+        elif menu_choice == "U":
+            pass
+        else:
+            print("Invalid input, please try again")
+            print(MENU)
+            menu_choice = input(">>>").upper()
 
 
 def load_projects(filename):
     """Load projects from a given filename"""
-    pass
+    projects = []
+    with open(filename, "r") as in_file:
+        for line in in_file:
+            print(line)
+            projects.append(line)
+    return projects
 
-def save_projects(filname):
+
+def save_projects(filename):
     """Save projects to a given filename"""
     pass
+
 
 def dispay_projectS():
     """Display incomplete and complete projects, sorted by priority"""
     pass
 
+
 def filter_projects_by_date(date):
     """Return a list of projects sorted by date that start after a given date"""
     pass
+
 
 def add_new_project():
     """Add a new project to memory given inputs"""
     pass
 
+
 def update_project():
     """Modify completion or priority of an established project"""
     pass
+
 
 main()
