d87e08a24d8acb285be3b6cd8dc9337a46dfa345
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2023-10-31 13:37:58 +1000

    Add function headers to Project Management Exercise

---
+++ prac_07/project.py
@@ -0,0 +1,14 @@
+"""
+CP1404/CP5632 Practical
+Project
+
+Estimated: 1 hour
+Actual:
+"""
+
+class Project:
+    """Represent a Project"""
+    def __init__(self):
+        """Initialise a Project"""
+        pass
+
---
+++ prac_07/project_management.py
@@ -0,0 +1,49 @@
+"""
+CP1404/CP5632 Practical
+Project Management
+
+Estimated: 1 hour
+Actual:
+
+"""
+
+MENU = """
+- (L)oad projects  
+- (S)ave projects  
+- (D)isplay projects  
+- (F)ilter projects by date
+- (A)dd new project  
+- (U)pdate project
+- (Q)uit
+"""
+
+def main():
+    """Main program for Project Managment"""
+    pass
+
+
+def load_projects(filename):
+    """Load projects from a given filename"""
+    pass
+
+def save_projects(filname):
+    """Save projects to a given filename"""
+    pass
+
+def dispay_projectS():
+    """Display incomplete and complete projects, sorted by priority"""
+    pass
+
+def filter_projects_by_date(date):
+    """Return a list of projects sorted by date that start after a given date"""
+    pass
+
+def add_new_project():
+    """Add a new project to memory given inputs"""
+    pass
+
+def update_project():
+    """Modify completion or priority of an established project"""
+    pass
+
+main()
