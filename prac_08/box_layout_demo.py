3490cc714374588ebdb2543c6995e7a7b4da6c51
Sachh Ariel Moka <sachhariel.moka@my.jcu.edu.au>
2023-11-08 11:13:14 +1000

    Update module docstring

--- prac_08/box_layout_demo.py
+++ prac_08/box_layout_demo.py
@@ -4,14 +4,17 @@ from kivy.lang import Builder
 
 class BoxLayoutDemo(App):
     def build(self):
+        """ build the Kivy app from the kv file """
         self.title = "Box Layout Demo"
         self.root = Builder.load_file('box_layout.kv')
         return self.root
 
     def handle_greet(self):
+        """ handle the greeting button"""
         self.root.ids.output_label.text = f"Hello {self.root.ids.input_name.text}"
 
     def handle_clear(self):
+        """ handle the clear button"""
         self.root.ids.output_label.text = ""
         self.root.ids.input_name.text = ""
 
